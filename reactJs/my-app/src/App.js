7a8b688e94bdaa4299ba5bc2e50e4a699f44d672
Sarthak M. Wagh <sarthakwagh16@gmail.com>
2023-09-04 11:09:16 +0530

    React day0 2

--- reactJs/my-app/src/App.js
+++ reactJs/my-app/src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import {Greet} from './components/first_compo'
-function Mybtn(){
-  return(
+import './App.css';
+//import {Greet} from './components/first_compo';
 
-    <button>I am a button !</button>
-  );
+//topics= folder structure, basic info about react, how to create component
+// how to write jsx , 
+
+const name=()=>{
+  alert('hello react');
 }
+
+const greet=<h2>Good morning folks !</h2>
+
+
+const age=(a)=>{
+  if(a>18){
+    return <h2>you are eligible for voting</h2>
+  }
+  else{
+    return <h2>you are not eligible for voting</h2>
+  }
+}
+
+const intro="Sarthak";
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my first react app !!</h1>
-      <Mybtn />
-      <Greet />
+      <h1>Welcome to my first react app !</h1>
+      {greet}
+      <button onClick={name}>Btn</button>
+      {age(19)}
+      <h1>Hello {intro}</h1>
     </div>
   );
 }
